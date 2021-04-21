347a38f3154b9ec0964f6c7f90efb3368922e6c3
Anthony Lazaro <50080273+AFLazaro@users.noreply.github.com>
2021-04-20 20:21:17 -0400

    Update script.js
    Updates related to revamp of resume section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,34 @@
 ////// SELECTORS
-// "Home" navigation section
+//// Home navigation section
 const navSection = document.querySelector(".nav");
-
-// "About Me" section
 const aboutButton = document.querySelector(".btn-about");
+const projectsButton = document.querySelector(".btn-projects");
+const resumeButton = document.querySelector(".btn-resume");
+
+//// About Me section
 const aboutSection = document.querySelector(".about");
 const aboutClose = document.querySelector(".about__close");
 
-// "Projects" section
-const projectsButton = document.querySelector(".btn-projects");
+//// Projects section
 const projectsSection = document.querySelector(".projects");
 const projectsClose = document.querySelector(".projects__close");
 
-// "Resume" section
-const resumeButton = document.querySelector(".btn-resume");
+//// Resume section
 const resumeSection = document.querySelector(".resume");
 const resumeClose = document.querySelector(".resume__close");
-const collapseResumeBtn = document.querySelector(".show-hide-btn");
+const collapseResumeBtn = document.querySelector(".show-hide-all");
 const resumeDisplay = document.querySelector(".resume__display");
+// Certification section
+const collapseCertsBtn = document.querySelector(".show-hide-certs");
+const certsSection = document.querySelector(".resume__display__certs");
+// Career section
+const collapseCareerBtn = document.querySelector(".show-hide-career");
+const careerSection = document.querySelector(".resume__display__career");
+// Education section
+const collapseEducationBtn = document.querySelector(".show-hide-education");
+const educationSection = document.querySelector(".resume__display__education");
+// Array containing all sections
+const resumeSections = [certsSection, careerSection, educationSection];
 
 
 
@@ -51,9 +62,21 @@ resumeButton.addEventListener("click", () => {
 resumeClose.addEventListener("click", () => {
     showHideSections(resumeSection);
 });
-// Collapse/Uncollapse Embedded Resume
+// Collapse All Resume Sections
 collapseResumeBtn.addEventListener("click", () => {
-    collapseResume();
+    collapseAllSections();
+});
+// Collapse Certifications
+collapseCertsBtn.addEventListener("click", () => {
+    collapseResumeSection(certsSection);
+});
+// Collapse Work Experience
+collapseCareerBtn.addEventListener("click", () => {
+    collapseResumeSection(careerSection);
+});
+// Collapse Education
+collapseEducationBtn.addEventListener("click", () => {
+    collapseResumeSection(educationSection);
 });
 
 
@@ -70,12 +93,12 @@ const showHideSections = (section) => {
         navSection.classList.remove("fadeinGrow");
         navSection.classList.add("fadeoutShrink");
         navSection.classList.add("hidden");
-        addTabNavigation(navSection);
+        removeTabNavigation(navSection);
     } else {
         section.classList.remove("slidein");
         section.classList.add("hidden");
         section.classList.add("slideout");
-        addTabNavigation(section);
+        removeTabNavigation(section);
 
         navSection.classList.remove("fadeinGrow");
         navSection.classList.add("fadeinGrow");
@@ -84,20 +107,41 @@ const showHideSections = (section) => {
     }
 };
 
-// Collapse and uncollapse the resume on the page.
-const collapseResume = () => {
-    if(resumeDisplay.classList.contains("collapsed")) {
-        resumeDisplay.classList.remove("collapse");
-        resumeDisplay.classList.remove("collapsed");
-        resumeDisplay.classList.add("uncollapse");
+// Collapse and uncollapse all resume sections.
+const collapseAllSections = () => {
+    if(certsSection.classList.contains("collapsed") || careerSection.classList.contains("collapsed") || educationSection.classList.contains("collapsed")) {
+        for(let i = 0; i < resumeSections.length; i++){
+            resumeSections[i].classList.remove("collapse");
+            resumeSections[i].classList.remove("collapsed");
+            resumeSections[i].classList.add("uncollapse");
+            resumeSections[i].classList.remove("remove-tab");
+        };
     } else {
-        resumeDisplay.classList.remove("uncollapse");
-        resumeDisplay.classList.add("collapse");
-        resumeDisplay.classList.add("collapsed");
-    }
+        for(let i = 0; i < resumeSections.length; i++){
+            resumeSections[i].classList.remove("uncollapse");
+            resumeSections[i].classList.add("collapse");
+            resumeSections[i].classList.add("collapsed");
+            removeTabNavigation(resumeSections[i]);
+        };
+    };
 };
 
-// Required in order to keep the transition animations while removing hidden sections from tab navigtion
-const addTabNavigation = (section) => {
+// Collapse and uncollapse individual resume sections.
+const collapseResumeSection = (resumeSection) => {
+    if(resumeSection.classList.contains("collapsed")) {
+        resumeSection.classList.remove("collapse");
+        resumeSection.classList.remove("collapsed");
+        resumeSection.classList.add("uncollapse");
+        resumeSection.classList.remove("remove-tab");
+    } else {
+        resumeSection.classList.remove("uncollapse");
+        resumeSection.classList.add("collapse");
+        resumeSection.classList.add("collapsed");
+        removeTabNavigation(resumeSection);
+    };
+};
+
+// Required in order to keep the transition animations while removing hidden sections from tab navigtion.
+const removeTabNavigation = (section) => {
     setTimeout(function(){ section.classList.add("remove-tab"); }, 400)
 };
